df363720fd65f403085206e562b5a2ae0a363918
Sonali Hirave <hiravesonali@gmail.com>
2026-05-20 11:47:47 +0200

    added debauncing functionality using use-debounce library

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  // Debouncing - useDebouncedCallback()
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log('searching.. ', term);
 
     const params = new URLSearchParams(searchParams);
@@ -18,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">

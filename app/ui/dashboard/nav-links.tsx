27ab8216bb58f09ca62f3cbab956ec3b918ee14b
Sonali Hirave <hiravesonali@gmail.com>
2026-05-30 10:49:13 +0200

    update the primary color and brand images

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,7 +31,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-emerald-100 hover:text-emerald-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-emerald-100 hover:text-emerald-600 bg-gray-50 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-emerald-50 text-emerald-700': pathname === link.href,
               },

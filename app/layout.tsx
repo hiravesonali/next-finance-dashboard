231e1c65b6722df888008fe4653dda71298c22b6
Sonali Hirave <hiravesonali@gmail.com>
2026-06-05 09:54:31 +0200

    Added meta data to the project and update the fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | FinDesk Dashboard',
+    default: 'Findesk Dashboard',
+  },
+  description: 'The Findesk dashboard to manage your finance in one place.',
+  metadataBase: new URL('https://findsk.vercel.app'),
+};
 
 export default function RootLayout({
   children,
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
 import FindeskLogo from '@/app/ui/findesk-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
 
 export default function LoginPage() {
   return (
